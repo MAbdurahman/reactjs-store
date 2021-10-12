7420f19115be314bbd9600b0ef13331247ec7238
MAbdurahman <mdbdrrhm@gmail.com>
2021-10-12 04:22:55 -0500

    Complete FeaturedProduct setup

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import { useProductsContext } from '../context/products_context'
-import { Link } from 'react-router-dom'
-import styled from 'styled-components'
-import Error from './Error'
-import Loading from './Loading'
-import Product from './Product'
+import React from 'react';
+import { useProductsContext } from '../context/products_context';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Error from './Error';
+import Loading from './Loading';
+import Product from './Product';
 
 const FeaturedProducts = () => {
 	//**************** variables ****************//
@@ -35,29 +35,29 @@ const FeaturedProducts = () => {
 			</Link>
 		</Wrapper>
 	);
-}
+};
 
 const Wrapper = styled.section`
-  background: var(--clr-grey-10);
-  .featured {
-    margin: 4rem auto;
-    display: grid;
-    gap: 2.5rem;
-    img {
-      height: 225px;
-    }
-  }
-  .btn {
-    display: block;
-    width: 148px;
-    margin: 0 auto;
-    text-align: center;
-  }
-  @media (min-width: 576px) {
-    .featured {
-      grid-template-columns: repeat(auto-fit, minmax(360px, 1fr));
-    }
-  }
-`
+	background: var(--clr-grey-10);
+	.featured {
+		margin: 4rem auto;
+		display: grid;
+		gap: 2.5rem;
+		img {
+			height: 225px;
+		}
+	}
+	.btn {
+		display: block;
+		width: 148px;
+		margin: 0 auto;
+		text-align: center;
+	}
+	@media (min-width: 576px) {
+		.featured {
+			grid-template-columns: repeat(auto-fit, minmax(360px, 1fr));
+		}
+	}
+`;
 
-export default FeaturedProducts
+export default FeaturedProducts;
--- src/context/products_context.js
+++ src/context/products_context.js
@@ -45,6 +45,9 @@ export const ProductsProvider = ({ children }) => {
 		}
 	};
 
+	useEffect(() => {
+		fetchProducts(url)
+	}, []);
 	return (
 		<ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar }}>
 			{children}
