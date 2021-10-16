e89bf0455348b3d1adf6a496f73f7001bdb99774
MAbdurahman <mdbdrrhm@gmail.com>
2021-10-16 05:30:57 -0500

    Add a hot loader to index.js

--- src/index.js
+++ src/index.js
@@ -8,15 +8,32 @@ import { CartProvider } from './context/cart_context';
 import { UserProvider } from './context/user_context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
-ReactDOM.render(
-	<React.StrictMode>
-		<ProductsProvider>
-			<FilterProvider>
-				<App />
-			</FilterProvider>
-		</ProductsProvider>
-	</React.StrictMode>,
-	document.getElementById('root')
-);
+
+
+const root = document.getElementById('root');
+
+
+function render() {
+
+	ReactDOM.render(
+		<React.StrictMode>
+			<ProductsProvider>
+				<FilterProvider>
+					<App />
+				</FilterProvider>
+			</ProductsProvider>
+		</React.StrictMode>,
+		document.getElementById('root')
+	);
+
+}
+
+if (module.hot) {
+	module.hot.accept('./app/App', function () {
+		setTimeout(render);
+	});
+}
+
+render();
 
 
