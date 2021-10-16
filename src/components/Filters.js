4742826c4d2cbd337612501e41c2a803b5baf63d
MAbdurahman <mdbdrrhm@gmail.com>
2021-10-16 05:47:26 -0500

    Add free shipping to Filter.js

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -132,6 +132,17 @@ const Filters = () => {
 							value={price}
 						/>
 					</div>
+					{/* shipping */}
+					<div className='form-control shipping'>
+						<label htmlFor='shipping'> free shipping</label>
+						<input
+							type='checkbox'
+							name='shipping'
+							id='shipping'
+							onChange={updateFilters}
+							checked={shipping}
+						/>
+					</div>
 				</form>
 			</div>
 		</Wrapper>
