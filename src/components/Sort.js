079aa89f5afd6bc941a0aa27bd4bc0380e55b58a
MAbdurahman <mdbdrrhm@gmail.com>
2021-10-16 06:47:58 -0500

    Add 'products && products.length' and etc to stop error

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -30,7 +30,7 @@ const Sort = () => {
 					<BsList />
 				</button>
 			</div>
-			<p>{products.length} products found</p>
+			<p>{products && products.length} products found</p>
 			<hr />
 			<form>
 				<label htmlFor='sort'>sort by</label>
