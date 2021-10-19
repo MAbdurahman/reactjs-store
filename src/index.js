6859a646ef5cdd238aa6e58de528a465bfacb0f7
MAbdurahman <mdbdrrhm@gmail.com>
2021-10-19 10:22:13 -0500

    Complete setup for Auth0 in index.js

--- src/index.js
+++ src/index.js
@@ -11,20 +11,25 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const root = document.getElementById('root');
 
 function render() {
-
 	ReactDOM.render(
 		<React.StrictMode>
-			<ProductsProvider>
-				<FilterProvider>
-					<CartProvider>
-						<App />
-					</CartProvider>
-				</FilterProvider>
-			</ProductsProvider>
+			<Auth0Provider
+				domain={process.env.REACT_APP_AUTH_DOMAIN}
+				clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
+				redirectUri={window.location.origin}
+				cacheLocation='localstorage'
+			>
+				<ProductsProvider>
+					<FilterProvider>
+						<CartProvider>
+							<App />
+						</CartProvider>
+					</FilterProvider>
+				</ProductsProvider>
+			</Auth0Provider>
 		</React.StrictMode>,
 		document.getElementById('root')
 	);
-
 }
 
 if (module.hot) {
@@ -34,5 +39,3 @@ if (module.hot) {
 }
 
 render();
-
-
