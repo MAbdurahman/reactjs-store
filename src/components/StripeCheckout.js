6f4c779f2606c8413f720b1919f331b79efcfb76
MAbdurahman <mdbdrrhm@gmail.com>
2021-10-21 08:23:06 -0500

    Adjustment to StripeCheckout form

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -137,7 +137,7 @@ const CheckoutForm = () => {
 					Payment succeeded, click to go to home page
 					<a href='/'>
 						{' '}
-						Stripe dashboard.
+						Home Page
 					</a>{' '}
 					Refresh the page to pay again.
 				</p>
@@ -158,7 +158,7 @@ const StripeCheckout = () => {
 
 const Wrapper = styled.section`
 	form {
-		width: 30vw;
+		width: 50vw;
 		align-self: center;
 		box-shadow: 0px 0px 0px 0.5px rgba(50, 50, 93, 0.1),
 			0px 2px 5px 0px rgba(50, 50, 93, 0.1),
